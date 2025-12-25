9a3fad9b981955d7897682cc424fc4d65413089e
JJ-ames <mirhoknmah@gmail.com>
2025-12-25 20:05:02 +0900

    Add prime number calculation and output

---
+++ prime.py
@@ -0,0 +1,10 @@
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+primes = [num for num in range(1, 101) if is_prime(num)]
+print(primes)
